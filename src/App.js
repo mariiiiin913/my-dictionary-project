34856b80fcfa8c94fb308723d6380f7323bceb42
Marin.R <88306713+mariiiiin913@users.noreply.github.com>
2022-01-07 00:39:14 +0900

    Added Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        Hello
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+
+      </footer>
+      </div>
     </div>
   );
 }
