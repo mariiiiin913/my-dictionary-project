e10e583ebcdc0bd3723aa450b73afa6a998c9140
Marin.R <88306713+mariiiiin913@users.noreply.github.com>
2022-01-08 00:07:28 +0900

    Added api documentation

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,7 +11,8 @@ const[keyword, setKeyword]=useState("");
 
   function search(event){
     event.preventDefault(); 
-
+    
+    ///documentation: https://dictionaryapi.dev/
     let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
