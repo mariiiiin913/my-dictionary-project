d0f3b70b944aacea49ed5e08b68a5f5045054575
Marin.R <88306713+mariiiiin913@users.noreply.github.com>
2022-01-08 00:05:23 +0900

    Added apiUrl

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary(){
 const[keyword, setKeyword]=useState("");
 
+  function handleResponse(response){
+    console.log(response.data[0]);
+  }
+
   function search(event){
-    event.preventDefault();
-    alert(`Searching for ${keyword} `)
+    event.preventDefault(); 
+
+    let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordUpdate(event){
