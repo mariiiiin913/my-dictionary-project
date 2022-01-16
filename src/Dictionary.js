dd11b80405bd61f0e0ae494b788c570351deff54
Marin.R <88306713+mariiiiin913@users.noreply.github.com>
2022-01-17 00:29:36 +0900

    Updated Dictionary.js
    Added props, useState(load), search(), load() and handleSubmit

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,36 +3,50 @@ import React, { useState } from "react";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary(){
-const[keyword, setKeyword]=useState("");
+export default function Dictionary(props){
+const[keyword, setKeyword]=useState(props.defaultKeyword);
 const[results, setResults]=useState(null);
+const[loaded,setLoaded]=useState(false);
 
   function handleResponse(response){
     setResults(response.data[0]);
   }
 
-  function search(event){
-    event.preventDefault(); 
-    
+  function search(){
     ///documentation: https://dictionaryapi.dev/
     let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event){
+    event.preventDefault(); 
+    search();
   }
 
   function handleKeywordUpdate(event){
     setKeyword(event.target.value);
   }
 
+  function load(){
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded){
     return (
         <div className="Dictionary">
           <section>
           <div className="hint">
               Look up a word...
             </div>
-          <form onSubmit={search}>
+          <form onSubmit={handleSubmit}>
             <input type="search" onChange={handleKeywordUpdate}/>
             </form>
             </section>
             <Results results={results}/>
             </div>);
-}
+        }else{
+          load();
+          return "Loading..."
+        }
+    }
