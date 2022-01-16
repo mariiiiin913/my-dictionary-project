51a44526729cbd9af0f1f00adb0def8535e472fd
Marin.R <88306713+mariiiiin913@users.noreply.github.com>
2022-01-16 20:00:40 +0900

    Added profile link

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-      </header>
+        <header className="App-header">
+        </header>
       <main>
         <Dictionary />
       </main>
       <footer>
-         <p className='myName'>Coded by Marin Rei</p>
+         <p className='myName'>Coded by <a href="https://www.linkedin.com/in/marin-rei-70260690/" target="_blank" rel="noreferrer"> Marin Rei</a></p>
       </footer>
       </div>
     </div>
