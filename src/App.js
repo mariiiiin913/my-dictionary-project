27e8c7d09cb9b3745e6b60a69cbe72acb9a418ca
Marin.R <88306713+mariiiiin913@users.noreply.github.com>
2022-01-18 00:11:37 +0900

    Added h1

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
+          <h1>Dictionary</h1>
         </header>
       <main>
-        <Dictionary defaultKeyword="dictionary"/>
+        <Dictionary defaultKeyword="smile"/>
       </main>
       <footer>
          <p className='myName'>Coded by <a href="https://www.linkedin.com/in/marin-rei-70260690/" target="_blank" rel="noreferrer" className="profile-link"> Marin Rei</a></p>
