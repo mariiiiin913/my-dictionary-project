3d6e7fbe2994fd54786352b959ca5758f0128bdf
Marin.R <88306713+mariiiiin913@users.noreply.github.com>
2022-01-18 00:10:22 +0900

    Created Photos component

---
+++ src/Photos.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props){
+    if(props.photos) {
+    return <section className="Photos">
+        <div className="row">
+        {props.photos.map(function(photo,index){
+            return(
+                <div className="col-4" key={index}>
+                <img src={photo.src.tiny} alt="result"/>
+                </div>
+            );
+        })}
+        </div>
+    </section>;
+    }else{
+        return null;
+    }
+}
