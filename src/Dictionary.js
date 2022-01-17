a0972f46553cd43ef8e39db3121e28973f0e4f0a
Marin.R <88306713+mariiiiin913@users.noreply.github.com>
2022-01-18 00:11:20 +0900

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,32 @@
 import axios from "axios";
 import React, { useState } from "react";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props){
 const[keyword, setKeyword]=useState(props.defaultKeyword);
 const[results, setResults]=useState(null);
 const[loaded,setLoaded]=useState(false);
+const[photos,setPhotos]=useState(null);
 
-  function handleResponse(response){
+  function handleDictionResponse(response){
     setResults(response.data[0]);
+  }
+
+  function handlePexcelsResponse(response){
+   setPhotos(response.data.photos);
   }
 
   function search(){
     ///documentation: https://dictionaryapi.dev/
     let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexcelsApiKey="563492ad6f91700001000001ceedf5a058514a3ea46c654437750eca";
+    let pexcelsApiUrl=`https://api.pexels.com/v1/search?query=${keyword}&per_page=80`;
+    let headers={Authorization: `Bearer ${pexcelsApiKey}`}
+    axios.get(pexcelsApiUrl, {headers: headers}).then(handlePexcelsResponse);
   }
 
   function handleSubmit(event){
@@ -44,6 +55,7 @@ const[loaded,setLoaded]=useState(false);
             </form>
             </section>
             <Results results={results}/>
+            <Photos photos={photos} />
             </div>);
         }else{
           load();
