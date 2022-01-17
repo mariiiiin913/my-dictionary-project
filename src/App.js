4a9d2e1b6719caf4fa632cef39f7f65794759264
Marin.R <88306713+mariiiiin913@users.noreply.github.com>
2022-01-18 01:57:01 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <footer>
         <p className="myName">This project was coded by {""}
         <a href='https://www.linkedin.com/in/marin-rei-70260690/' target="_brank" className="profile-link" rel='norefferer'>Marin Rei</a> 
-        {""} and is{""} <a href="https://github.com/mariiiiin913/my-dictionary-project" target="_brank" rel='norefferer' className='github-link'>open-sorced on GitHub</a> and <a href='https://app.netlify.com/teams/mariiiiin913/overview' target="_brank" className="netlify-link" rel='norefferer'>hosted on Netlify</a> </p>
+        {""} and is{""} <a href="https://github.com/mariiiiin913/my-dictionary-project" target="_brank" rel='norefferer' className='github-link'>open-sorced on GitHub</a> and <a href='https://wonderful-sinoussi-8d8043.netlify.app' target="_brank" className="netlify-link" rel='norefferer'>hosted on Netlify</a> </p>
       </footer>
       </div>
     </div>
